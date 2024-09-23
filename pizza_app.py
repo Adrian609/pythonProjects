5147927add7a321738c54aad9554e754bf9b0857
adrian <arod609@outlook.com>
2024-09-23 13:44:02 -0600

    added conditions and additional key to pizzas and topping

--- pizza_app.py
+++ pizza_app.py
@@ -1,22 +1,27 @@
-
 SALES_TAX_RATE = 0.04
 EXTRA_SAUCE_COST = 0.50
 
 pizza_prices = {
-    'small': 7.00,
-    'medium': 10.75,
-    'large': 14.75
+    'small' : 7.00,
+    's': 7.00,
+    'medium' : 10.75,
+    'm': 10.75,
+    'large' : 14.75,
+    'l': 14.75
 }
 
 topping_prices = {
     'small': 0.50,
-    'medium': 1.00,
-    'large': 1.50
+    's': 0.50,
+    'medium' : 1.00,
+    'm': 1.00,
+    'large': 1.50,
+    'l': 1.50
 }
 
 subtotal = 0.0
 count = 0
-number_pizzas = int(input("How many pizzas? "))
+number_pizzas = int(input("How many pizzas? ").strip())
 
 while number_pizzas > 0:
     count += 1
@@ -29,7 +34,7 @@ while number_pizzas > 0:
 
     order_description = f'A {size_pizza.capitalize()} pizza with {num_toppings} toppings'
 
-    if extra_sauce == 'yes' or 'y':
+    if extra_sauce in ('yes' or 'y'):
         temp_total += EXTRA_SAUCE_COST
         subtotal += EXTRA_SAUCE_COST
         order_description += f' and extra sauce'
@@ -41,6 +46,6 @@ while number_pizzas > 0:
 tax_amount = subtotal * SALES_TAX_RATE
 total = subtotal + tax_amount
 
-print(f'Subtotal:      ${subtotal:.2f}')
-print(f'Tax (4%):      ${tax_amount:.2f}')
-print(f'Total:         ${total:.2f}')
+print(f'Subtotal:      ${subtotal:.2f}\n'
+      f'Tax (4%):      ${tax_amount:.2f}\n'
+      f'Total:         ${total:.2f}\n')
