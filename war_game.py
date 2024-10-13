84c67505e864b083b068fcec712898bda293d018
adrian <arod609@outlook.com>
2024-10-12 18:38:13 -0600

    added functionality to play a hand and play multiple hands. Need to add a functionality if the players hit a draw

--- war_game.py
+++ war_game.py
@@ -1,47 +1,65 @@
 import random
 
+
 def build_deck():
     suits = ['H', 'D', 'C', 'S']  # Hearts, Diamonds, Clubs, Spades
-    values = list(range(2, 15))    # 2-10, J, Q, K, A (where A=14)
+    values = list(range(2, 15))  # 2-10, J, Q, K, A (where A=14)
     deck = [(value, suit) for suit in suits for value in values]
     random.shuffle(deck)
     return deck
 
+
 def build_hand(card_deck):
-    hand=[]
-    for i in range(0, 14):
-        hand.append(card_deck[i])
-        deck.pop(i)
+    hand = []
+    for i in range(0, 26):
+        hand.append(card_deck.pop())
     return hand
 
-def play_game(player_one_hand, player_two_hand):
-    player_one_wins = 0
-    player_two_wins = 0
-    for i in range(0, len(player_one_hand)):
-        if player_one_hand[i] > player_two_hand[i]:
-            player_one_wins += 1
-            print(f"{player_one_hand[i]} {player_two_hand[i]} player one wins {player_one_wins} : {player_two_wins}")
 
-        elif player_two_hand[i] > player_one_hand[i]:
-            player_two_wins += 1
-            print(f"{player_one_hand[i]} + {player_two_hand[i]} player two wins {player_one_wins} : {player_two_wins}")
+def play_hand(hand1, hand2):
+    common_pool = [hand1.pop(0), hand2.pop(0)]
+    winner = check_winner(common_pool[0], common_pool[1])
 
+    if winner == 1:
+        random.shuffle(common_pool)  # Shuffle the pool to avoid infinite loops
+        hand1.extend(common_pool)  # Add all cards in the pool to player 1
+    elif winner == 2:
+        random.shuffle(common_pool)
+        hand2.extend(common_pool)
 
-    if player_one_wins > player_two_wins:
-        return f"player_one_wins {player_one_wins} : {player_two_wins}"
-    elif player_two_wins == player_one_wins:
-        return f"Tie {player_two_wins} : {player_one_wins}"
-    return f"player_two_wins {player_two_wins} : {player_one_wins}"
+    return hand1, hand2
 
 
+def play_hands(player_one, player_two):
+    while len(player_one) != 0 and len(player_two) != 0:
+        play_hand(player_one, player_two)
+        if len(player_one) == 0:
+            return "player one lost"
+        if len(player_two) == 0:
+            return "player two lost"
+
+
+def check_winner(card1, card2):
+    if card1 == card2:
+        return 0
+    elif card1 > card2:
+        return 1
+    elif card1 < card2:
+        return 2
 
 deck = build_deck()
 player_one = build_hand(deck)
 player_two = build_hand(deck)
 
+print(play_hands(player_one, player_two))
+print(f'player1: {len(player_one)}')
+print(f'Player2: {len(player_two)}')
 
-#print(player_one)
-#print(player_two)
-#print(len(deck))
 
-print(play_game(player_one, player_two))
+def test_check_winner():
+    card1 = [(14, "H")]
+    card2 = [(13, "H")]
+    if check_winner(card1, card2) != 1:
+        print(f"Error: expected player1 to win")
+    else:
+        print(f"test passed: player 1 wins")
