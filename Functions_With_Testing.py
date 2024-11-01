b0140f48490b33557eecf8369e8611c9a8a7c7b3
adrian <arod609@outlook.com>
2024-10-31 18:05:51 -0600

    Added more test

--- Functions_With_Testing.py
+++ Functions_With_Testing.py
@@ -34,7 +34,6 @@ def all_pairs(x, y):
         for j in range(len(x) - 1):
             if x[i] != y[j]:
                 lst += [x[i], y[j]]
-
     return lst
 
 
@@ -42,7 +41,6 @@ def list_to_dict(lst):
     dict = {}
     for i in range(len(lst)):
         dict[i + 1] = lst[i]
-
     return dict
 
 
@@ -55,9 +53,12 @@ def invert_dict(dict):
 
 def adds_to_target(list, target):
     set_list = set(list)
-    for i in range(len(list)):
-        for j in range(len(list)):
-            if set_list(i) + set_list.pop(j) == target:
+    check_val = 0
+    for item in set_list:
+        for second_item in set_list:
+            if item != second_item:
+                check_val = item + second_item
+            if check_val == target:
                 return True
     return False
 
@@ -78,9 +79,20 @@ class MyTestCase(unittest.TestCase):
     def test_list_to_dict(self):
         lst = [2, 6, 6, 1, 7, 9]
         self.assertEqual(list_to_dict(lst), {1: 2, 2: 6, 3: 6, 4: 1, 5: 7, 6: 9})
+        self.assertEqual(
+            list_to_dict([4, 5, 6, 7, 8, ""]), {1: 4, 2: 5, 3: 6, 4: 7, 5: 8, 6: ""}
+        )
+        self.assertEqual([], [])
 
     def test_invert_dict(self):
+        self.assertEqual(invert_dict({}), {})
         self.assertEqual(invert_dict({1: 2, 2: 3, 3: 4}), {2: 1, 3: 2, 4: 3})
+        self.assertEqual(invert_dict({1: 2, 2: 0, 3: 0}), {2: 1, 0: 2, 0: 3})
+        self.assertNotEquals(invert_dict({1: 2, 2: 3, 3: 4}), {1: 2, 2: 3, 3: 4})
 
     def test_adds_to_target(self):
+        self.assertEqual(adds_to_target([], 14), False)
         self.assertEqual(adds_to_target([1, 6, 7, 3, 7, 10, 3], 13), True)
+        self.assertEqual(adds_to_target([1, 6, 7, 3, 6, 8, 3], 14), True)
+        self.assertEqual(adds_to_target([1, 2, 3, 4, 5, 8, 3], 14), False)
+        self.assertEqual(adds_to_target([1], 14), False)
